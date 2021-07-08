f74367f1cfbb28fad5242dd3c13b76ae1b174643
Nicole Uy <marvienicolebtw@hotmail.com>
2021-07-08 11:00:16 +0200

    fixing counter.test.js

--- test/counter.test.js
+++ test/counter.test.js
@@ -4,7 +4,7 @@ import {Counter} from "../src/js/counter";
 describe("Counter", () =>{
     it("should count numbers", () => {
 
-        let counter = new Counter()
+        let Counter = new counter()
 
         expect(counter.incrementAndReturn()).toBe(1)
         expect(counter.incrementAndReturn()).toBe(2)
